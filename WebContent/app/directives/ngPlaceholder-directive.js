bc2e9c50d8d4f25c33c1855b1b4ad77a16759893
Jitendra <jitendra8911@gmail.com>
2015-08-16 01:13:09 -0400

    added custom directive for placeholder in the form element called ngPlaceholder

---
+++ WebContent/app/directives/ngPlaceholder-directive.js
@@ -0,0 +1,18 @@
+(function()
+		{
+	
+	 angular.module('restaurantReservationSystem')
+	 .directive('ngPlaceholder', function() {
+		  return {
+		    restrict: 'A',
+		    scope: {
+		      placeholder: '=ngPlaceholder'
+		    },
+		    link: function(scope, elem, attr) {
+		      scope.$watch('placeholder',function() {
+		        elem[0].placeholder = scope.placeholder; 
+		      });
+		    }
+		  }
+		});
+		})()
